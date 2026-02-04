b6970762dba33b9951fc8a02360073d6e6d8608b
sebotseleah-lab <sebotseleah97@gmail.com>
2026-02-04 15:34:39 +0200

    Added slides

---
+++ DevGearStore/wwwroot/js/autoSlider.js
@@ -0,0 +1,14 @@
+﻿const slider = document.getElementById("productSlider");
+
+let scrollStep = 1;
+let maxScroll = slider.scrollWidth - slider.clientWidth;
+
+function autoSlide() {
+    slider.scrollLeft += scrollStep;
+
+    if (slider.scrollLeft >= maxScroll) {
+        slider.scrollLeft = 0;
+    }
+}
+
+setInterval(autoSlide, 20); // smaller = smoother
---
+++ DevGearStore/wwwroot/js/slider.js
@@ -0,0 +1,25 @@
+﻿let slider;
+let scrollStep = 1;
+let intervalTime = 20;
+
+function autoSlide() {
+    if (!slider) return;
+
+    slider.scrollLeft += scrollStep;
+
+    // When it reaches the end, start again
+    if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth) {
+        slider.scrollLeft = 0;
+    }
+}
+
+// Make sure the page loads first
+window.onload = () => {
+    slider = document.getElementById("productSlider");
+
+    if (slider) {
+        setInterval(autoSlide, intervalTime);
+    } else {
+        console.log("Slider element not found!");
+    }
+};
